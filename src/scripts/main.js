464b849d859fc255765a0cbce6048e58ace5c090
Soboleva Tatiana <tanya_sobol@ukr.net>
2023-01-16 16:06:33 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const table = document.body.firstElementChild;
+
+for (let i = 0; i < table.rows.length; i++) {
+  let el;
+
+  if (i === 0 || i === table.rows.length - 1) {
+    el = document.createElement('th');
+  } else {
+    el = document.createElement('td');
+  }
+  el.innerText = table.rows[i].cells[1].innerText;
+
+  const el2 = table.rows[i];
+
+  el2.insertBefore(el, el2.children[4]);
+}
